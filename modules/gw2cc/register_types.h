f509a2df6a1de25ce1fff2318837362c0e5a975b
ACB <ansas.bogdan@rwth-aachen.de>
2022-08-05 06:05:34 +0200

    feat: linux compile

--- modules/gw2cc/register_types.h
+++ modules/gw2cc/register_types.h
@@ -1,4 +1,10 @@
 // Fake module so godot thinks there is a gw2cc module.
 
-void register_gw2cc_types();
-void unregister_gw2cc_types();
+#ifdef WIN32
+#define USED
+#else
+#define USED __attribute__((used))
+#endif
+
+void register_gw2cc_types() USED;
+void unregister_gw2cc_types() USED;
