174529d96e01f5831146a457d94c8db910c88c43
ACB <ansas.bogdan@rwth-aachen.de>
2024-04-20 18:10:02 +0200

    fix: reapply input patch

--- scene/main/viewport.cpp
+++ scene/main/viewport.cpp
@@ -3099,7 +3099,10 @@ bool Viewport::_sub_windows_forward_input(const Ref<InputEvent> &p_event) {
 
 	gui.subwindow_focused->_window_input(ev);
 
-	return true;
+	if(!gui.subwindow_focused) {
+		return true;
+	}
+	return gui.subwindow_focused->is_input_handled() || gui.subwindow_focused->is_handling_input_locally();
 }
 
 void Viewport::_update_mouse_over() {
